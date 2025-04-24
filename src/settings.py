3e778cc88c39bb5e7cc566c9307c45580306761d
neepaw <bangerwockeez@gmail.com>
2025-04-24 23:08:21 +0300

    Add logging setup in settings

--- src/settings.py
+++ src/settings.py
@@ -3,6 +3,8 @@ from yandex_cloud_ml_sdk import YCloudML
 
 from dotenv import load_dotenv
 
+import logging
+
 load_dotenv()
 
 api_key = os.getenv("API_KEY")
@@ -10,3 +12,8 @@ folder_id = os.getenv("FOLDER_ID")
 
 sdk = YCloudML(folder_id=folder_id, auth=api_key)
 model = sdk.models.completions("yandexgpt", model_version="rc")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
